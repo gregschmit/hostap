a55345ab6f90f90c32686ee59aa84651c35df821
Gregory N. Schmit <schmitgreg@gmail.com>
2025-08-28 13:17:24 -0500

    Experiment: Set DF bit in client station packets.

--- src/ap/softgre.c
+++ src/ap/softgre.c
@@ -11,9 +11,77 @@
 #ifdef CONFIG_SOFTGRE
 
 #include "utils/common.h"
-#include "utils/eloop.h"
+#include "utils/crc32.h"
 #include "hostapd.h"
 #include "softgre.h"
+#include "../l2_packet/l2_packet.h"
+#include <netinet/ip.h>
+
+
+static be16 ip_checksum(const void *buf, size_t len)
+{
+	u32 sum = 0;
+	const u16 *pos;
+
+	for (pos = buf; len >= 2; len -= 2)
+		sum += ntohs(*pos++);
+	if (len)
+		sum += ntohs(*pos << 8);
+
+	sum = (sum >> 16) + (sum & 0xffff);
+	sum += sum >> 16;
+	return htons(~sum);
+}
+
+/**
+ * softgre_modify_df_bit - Modify Don't Fragment bit on IPv4 packets with checksum and FCS recalculation
+ * @buf: Ethernet frame buffer (including FCS if present)
+ * @len: Buffer length
+ * @set_df: 1 to set DF bit, 0 to clear DF bit
+ * Returns: 0 on success, -1 if not IPv4 or too small
+ */
+int softgre_modify_df_bit(u8 *buf, size_t len, int set_df)
+{
+	struct l2_ethhdr *eth;
+	struct ip *ip_hdr;
+	u16 old_frag, new_frag;
+	u32 *fcs_ptr;
+	u32 new_fcs;
+	size_t frame_len_without_fcs;
+	
+	/* Minimum size: Ethernet header + IP header + FCS */
+	if (len < sizeof(struct l2_ethhdr) + sizeof(struct ip) + 4)
+		return -1;
+	
+	eth = (struct l2_ethhdr *) buf;
+	if (ntohs(eth->h_proto) != 0x0800) /* Not IPv4 */
+		return -1;
+	
+	ip_hdr = (struct ip *)(buf + sizeof(struct l2_ethhdr));
+	
+	old_frag = ntohs(ip_hdr->ip_off);
+	if (set_df) {
+		new_frag = old_frag | IP_DF; /* Set Don't Fragment bit */
+	} else {
+		new_frag = old_frag & ~IP_DF; /* Clear Don't Fragment bit */
+	}
+	
+	if (old_frag != new_frag) {
+		ip_hdr->ip_off = htons(new_frag);
+		
+		/* Recalculate IP header checksum */
+		ip_hdr->ip_sum = 0;
+		ip_hdr->ip_sum = ip_checksum(ip_hdr, ip_hdr->ip_hl << 2);
+		
+		/* Recalculate FCS (assume FCS is the last 4 bytes) */
+		frame_len_without_fcs = len - 4;
+		new_fcs = ieee80211_crc32(buf, frame_len_without_fcs);
+		fcs_ptr = (u32 *)(buf + frame_len_without_fcs);
+		*fcs_ptr = htonl(new_fcs);
+	}
+	
+	return 0;
+}
 
 /**
  * softgre_init - Initialize SoftGRE for a hostapd instance
--- src/ap/softgre.h
+++ src/ap/softgre.h
@@ -15,21 +15,6 @@
 #include "utils/common.h"
 #include "hostapd.h"
 
-/* GRE header structure */
-struct gre_header {
-	u16 flags_version;
-	u16 protocol;
-} __attribute__((__packed__));
-
-#define GRE_FLAG_CHECKSUM	0x8000
-#define GRE_FLAG_ROUTING	0x4000  
-#define GRE_FLAG_KEY		0x2000
-#define GRE_FLAG_SEQ		0x1000
-#define GRE_FLAG_STRICT_ROUTE	0x0800
-#define GRE_FLAG_VERSION_MASK	0x0007
-
-#define GRE_PROTOCOL_ETH	0x6558
-#define GRE_VERSION		0x0000
 
 /* SoftGRE context structure */
 struct softgre_ctx {
@@ -40,6 +25,7 @@ struct softgre_ctx {
 /* Function declarations */
 int softgre_init(struct hostapd_data *hapd);
 void softgre_deinit(struct hostapd_data *hapd);
+int softgre_modify_df_bit(u8 *buf, size_t len, int set_df);
 
 #endif /* CONFIG_SOFTGRE */
 #endif /* SOFTGRE_H */
